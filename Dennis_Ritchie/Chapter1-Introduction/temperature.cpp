c30ac9dfbcb824b61f1be3b768609bdc678c1fca
Dharya Choudhary <98805670+dharyachoudhary@users.noreply.github.com>
2022-02-19 16:19:20 +0530

    Create temperature.cpp

---
+++ Dennis_Ritchie/Chapter1-Introduction/temperature.cpp
@@ -0,0 +1,18 @@
+// For Faherehit to celcius and celcius to Faherehit.
+# include <stdio.h>
+
+// 1.c = 33.8 faher
+int main () {
+
+    printf("Enter Faherehit temprature: ");
+    float Faherehit;
+    scanf("%f",&Faherehit);
+    printf ("\n%f",Faherehit);
+
+    float a = (Faherehit - 32) * (5.0 / 9.0);
+    
+    float c =  a;
+    float celcius = -((Faherehit-32) * 5)/17.22 * 9;     //32+5 (f - 32) X 5/9 )/-17.22
+     printf ("\n celcius: %f",a);
+}
+
