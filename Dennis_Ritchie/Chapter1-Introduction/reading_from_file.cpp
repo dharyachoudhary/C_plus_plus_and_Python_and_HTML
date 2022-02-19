d95a488b8ee4e0bb509d5c1d64c80bac61db7f5e
Dharya Choudhary <98805670+dharyachoudhary@users.noreply.github.com>
2022-02-19 16:13:11 +0530

    Create reading_from_file.cpp

---
+++ Dennis_Ritchie/Chapter1-Introduction/reading_from_file.cpp
@@ -0,0 +1,20 @@
+/*
+    !   Not
+    !=  Not equal
+    ==  equal
+    =   assign
+    *   multiply
+    /   Divide
+*/
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char c;
+    FILE* ptr = fopen("C:\\Users\\amitk\\dharya_code\\file.txt","r");
+    while ((c = fgetc(ptr)) != EOF) {      
+        putchar(c);
+    }
+    fclose(ptr);
+    return 0;
+}
