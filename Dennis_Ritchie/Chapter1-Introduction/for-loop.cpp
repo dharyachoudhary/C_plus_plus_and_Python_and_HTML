8a2ff56e72b63578661a450a4d6e2f7fdf591ca7
Dharya Choudhary <98805670+dharyachoudhary@users.noreply.github.com>
2022-02-19 16:50:43 +0530

    Create for-loop.cpp

---
+++ Dennis_Ritchie/Chapter1-Introduction/for-loop.cpp
@@ -0,0 +1,16 @@
+// For printing the table of five till fifty.
+#include <stdio.h>
+
+/*
+    fun(): This is function
+    fun(int a)  a is function argument
+    char *ptr;  ptr is pointer
+*/
+
+int main(int argc, char const *argv[])
+{
+    //for(initialization; Test; increment)   //loop
+    for (int i = 0; i < 55; i = i+5){
+        printf("%d ",i);     
+    }
+}
