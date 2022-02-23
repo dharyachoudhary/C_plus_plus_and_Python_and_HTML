f73b77fb92f868d995bab7b01a25b4434aa99542
Dharya Choudhary <98805670+dharyachoudhary@users.noreply.github.com>
2022-02-23 20:36:14 +0530

    Create count_character_and_lines_from_file.cpp

---
+++ Dennis_Ritchie/Chapter1-Introduction/count_character_and_lines_from_file.cpp
@@ -0,0 +1,30 @@
+
+/*
+    !   Not
+    !=  Not equal
+    ==  equal
+    =   assign
+    *   multiply
+    /   Divide
+*/
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char c;
+    int count = 0;
+    int lines = 1;
+    FILE* ptr = fopen("C:\\Users\\amitk\\dharya_code\\file.txt","r");
+    while ((c = fgetc(ptr)) != EOF) {      
+        putchar(c);
+        count++;            //count = count + 1;
+
+        if (c == '\n')
+            lines++;        //lines = lines + 1; 
+    }
+    fclose(ptr);
+    printf ("\n\nTotal characters=%d",count);
+    printf("\n\nTotal lines=%d",lines);
+    return 0;
+}
+// EOF means end of file
